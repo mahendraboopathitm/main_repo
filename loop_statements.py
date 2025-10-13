d655bd1fcbd1590bba24cd69444048d58ee59dc4
mahendraboopathitm <mahendraboopathitm@gmail.com>
2025-10-13 18:50:59 +0530

    Create loop_statements.py

---
+++ loop_statements.py
@@ -0,0 +1,10 @@
+count = 0
+while count < 5:
+    print(count)
+    count += 1  
+  
+num = 0
+while num != 5:
+    num = int(input("Enter a number (5 to stop): "))
+    print("You entered:", num)
+  
