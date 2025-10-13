30615263ef93ad84e423cc6fc258a633544564de
mahendraboopathitm <mahendraboopathitm@gmail.com>
2025-10-13 18:34:31 +0530

    Update variables_and_datatypes.py

--- variables_and_datatypes.py
+++ variables_and_datatypes.py
@@ -1,1 +1,41 @@
+name = "Alice"
+age = 20
+height = 5.5
+is_student = True
+print(name,age,height,is_student)
 
+
+int_num = 10          
+float_num = 3.14      
+complex_num = 2 + 3j  
+
+
+string_val = "diggibyte!"     
+list_val = [1, 2, 3, "data"]    
+tuple_val = (10, 20, "engineer")     
+range_val = range(5)           
+
+dict_val = {"name": "boopathi", "age": 20, "grade": "A"}  
+
+
+set_val = {1, 2, 3, 3, 4}         
+frozenset_val = frozenset([1, 2, 3])  
+
+
+bool_val = True  
+
+
+none_val = None   
+
+print("Integer:", int_num)
+print("Float:", float_num)
+print("Complex:", complex_num)
+print("String:", string_val)
+print("List:", list_val)
+print("Tuple:", tuple_val)
+print("Range:", list(range_val))  
+print("Dictionary:", dict_val)
+print("Set:", set_val)
+print("Frozenset:", frozenset_val)
+print("Boolean:", bool_val)
+print("NoneType:", none_val)
