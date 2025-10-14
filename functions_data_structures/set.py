6e41a25da7522b46c4b3d9a0f161c6823c82f45a
mahendraboopathitm <mahendraboopathitm@gmail.com>
2025-10-14 21:44:41 +0530

    Update set.py

--- functions_data_structures/set.py
+++ functions_data_structures/set.py
@@ -1,1 +1,17 @@
+a={1,2,3,4,5,5}
+print(a)
+b={4,5,6,7,8,9}
+print("unoin",a|b)
+print("intersection",a&b)
+print(a.difference(b))
+print(a.symmetric_difference(b))
+a.add(9)
+print(a)
+a.update(["mango","banana"])
+print(a)
+a.remove("banana")
+a.discard("kiwi")
+print(2 in a)
+for i in b:
+    print(i)
 
