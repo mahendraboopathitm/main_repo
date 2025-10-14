d2295e1e8cb913de9562dfd9b8663a7f57da20b3
mahendraboopathitm <mahendraboopathitm@gmail.com>
2025-10-14 21:37:14 +0530

    Update dictionaries.py

--- functions_data_structures/dictionaries.py
+++ functions_data_structures/dictionaries.py
@@ -1,1 +1,18 @@
+a={"name":"boopathi","age":22,"pos":"intern"}
+print(a)
 
+b=dict(name="mathesh",age=22,pos="intern")
+print(b)
+for i,j in a.items():
+    print(a[i])
+for i in a:
+    print(a[i])
+print(a.get("age"))
+a["grade"]="A"
+print(a)
+a["age"]=20
+print(a)
+print(a.pop("age"))
+print(a.popitem())
+print(a.keys())
+print(a.valus())
